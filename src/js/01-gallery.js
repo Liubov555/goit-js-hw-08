e6f3d51313ee01e71f67216f127405aa66963d30
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-09-03 14:24:27 +0300

    Виконане завдання 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const gallerySection = onCreateGalleryItems(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", gallerySection);
+
+function onCreateGalleryItems(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+        `
+    }).join("");
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+    captionPosition: 'bottom'
+});
