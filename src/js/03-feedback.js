ca30f67cbba0a6f31070560cc3b911cd5d457a15
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-09-03 19:45:58 +0300

    Виконане завдання 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import { throttle } from 'lodash';
 
+
+const refs = {
+    formEl: document.querySelector('form'),
+    textaria: document.querySelector('textarea')
+}
+
+const FORM_KEY = 'feedback-form-state';
+const inputSavedText = {};
+
+refs.formEl.addEventListener('input', throttle(onTextAreaInput, 500))
+
+refs.formEl.addEventListener('input', evt => {
+    inputSavedText[evt.target.name] = evt.target.value;
+    localStorage.setItem(inputSavedText, JSON.stringify(inputSavedText));
+})
+
+function onTextAreaInput(evt) {
+    const savedText = evt.target.value;
+    localStorage.setItem(FORM_KEY, savedText);
+}
